244f1b90be3bd57249c243c20296c254a0335d53
Amjad <amjad.aalharbi@gmail.com>
2021-11-21 12:31:53 +0300

    Create App.js

---
+++ imagepop/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home";
+
+function App() {
+   return (
+      <>
+         <h1 className="text-center text-info mt-3"> SERVICES </h1>
+         <Home />
+      </>
+   );
+}
+
+export default App;
